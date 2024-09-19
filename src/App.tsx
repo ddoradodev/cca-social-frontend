33b757d97eec794735ef01e2a56d4e5feaf94f3d
ddoradodev <diegodorado@zaphreen.com>
2024-09-19 15:25:47 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-400">
+        <p className="text-xl text-white">Welcome to CCA SOCIAL MEDIA </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
