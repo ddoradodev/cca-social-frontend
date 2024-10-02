1ad9e16de88b13bbb021410cc9c8831ec53f20fd
ddoradodev <diegodorado@zaphreen.com>
2024-10-02 19:54:39 +0200

    shad cn installation done

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,5 @@
+import CardExample from "./CardExample";
+
 const Home = () => {
   // name the a functional component using pascal case
   return (
@@ -5,6 +7,7 @@ const Home = () => {
       This is the Home
       <p>this is a name</p>
       <p>this is a lastname</p>
+      <CardExample />
     </div>
   );
 };
