ed490e68426144187018f74c960246f8e320c20c
Jdel37 <foreromorenojuandavid79@gmail.com>
2024-10-18 12:23:48 -0500

    fireball

--- src/Chantre.tsx
+++ src/Chantre.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CustomeCard } from '@/components/ui/Custome-card';
 import chantreImage from '../public/assets/chantre.jpeg'; // Import the image directly
-
+import { useEffect } from 'react';
+import axios from 'axios';
+import { Button } from "@/components/ui/button"
+import { ChevronRight } from "lucide-react"
 const ImageArray = [
   { id: 1, url: chantreImage, des: 'chantrees una putica' },
   { id: 2, url: chantreImage, des: 'chantrees una putica' },
@@ -9,12 +12,33 @@ const ImageArray = [
 ];
 
 function Chantre() {
+  const [personajes, setpersonajes] = useState([])
+  useEffect(() => {
+  axios.get("https://rickandmortyapi.com/api/character").then((chantre)=>setpersonajes(chantre.data.results))
+  }, [])
+  console.log(personajes)
   return (
-    <div>
-      {ImageArray.map((x) => (
-        <CustomeCard data={x} key={x.id} />
+<>
+<div>
+
+<Button variant="outline" size="icon">
+    <ChevronRight className="h-4 w-4" />
+  </Button>
+  <Button variant="outline" size="icon">
+    <ChevronRight className="h-4 w-4" />
+  </Button>
+</div>
+  
+
+    <div className='flex flex-col flex-wrap lg:flex-row'>
+      {personajes.map((x) => (
+        <div key={x.id}>
+<CustomeCard data={x}  />
+        </div>
+        
       ))}
     </div>
+    </>
   );
 }
 
--- src/components/ui/Custome-card.tsx
+++ src/components/ui/Custome-card.tsx
@@ -19,16 +19,16 @@ interface CustomeCardProps {
 
 export const CustomeCard: React.FC<CustomeCardProps> = ({ data }) => {
   return (
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mx-auto h-auto p-4">
-      <Card className="bg-white border border-gray-300 shadow-lg transition-transform transform hover:scale-105 duration-200">
+    <div className="w-[400px]  mx-auto h-auto p-4 ">
+      <Card className="bg-gradient-to-r from-cyan-500 to-blue-500 ... border border-gray-300 shadow-lg transition-transform transform hover:scale-105 duration-200">
         <CardHeader className="text-center">
-          <CardTitle className="text-xl font-semibold text-gray-800">{data.des}</CardTitle>
+          <CardTitle className="text-3xl font-semibold text-gray-800">{data.name}</CardTitle>
         </CardHeader>
         <CardContent className="flex justify-center">
-          <img src={data.url} alt="chantre" className="rounded-full w-32 h-32 border-4 border-gray-200 shadow-md" />
+          <img src={data.image} alt="chantre" className="rounded-full w-32 h-32 border-4 border-gray-200 shadow-md" />
         </CardContent>
         <CardFooter className="text-center">
-          <p className="text-gray-600">{data.des || 'Dorado el mejor'}</p>
+          <p className="text-gray-900 text-2xl  mx-auto">Specie: {data.species || 'Dorado el mejor'}</p>
         </CardFooter>
       </Card>
     </div>
